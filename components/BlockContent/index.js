c1a9f124cd97dd3b92cb69f65621a377e333be90
Nuril Baradusov <baradusovnh@gmail.com>
2020-11-17 18:32:42 +0400

    Добавил группированные блоки

--- components/BlockContent/index.js
+++ components/BlockContent/index.js
@@ -128,7 +128,8 @@ const BlockContent = (props) => {
             );
           }
           case 'start_game_btn': {
-            return <a href={`https://meduza.io/${block.data.material_url}`}>Играть на Медузе</a>;
+          case 'grouped': {
+            return <BlockContent blocks={block.data} />;
           }
           case 'embed': {
             return <Embed key={block.id} data={block.data} isVideo={isVideo} />;
