d048fccb196b73f04dcb87fcecc7bdcdd6f6b647
Nuril Baradusov <baradusovnh@gmail.com>
2021-06-15 14:13:51 +0400

    Добавил фоллбэк для инстаграм постов
    Фэйсбук опять всё усложнил

--- components/InstagramPost/index.js
+++ components/InstagramPost/index.js
@@ -4,16 +4,29 @@ const InstagramPost = (props) => {
   const { instagramPost } = props;
   const { instagramUrl, imageUrl, author } = instagramPost;
 
-  return (
-    <figure className={styles.figure}>
-      <a className={styles.link} href={instagramUrl}>
-        <img src={imageUrl} />
-      </a>
+  if (imageUrl) {
+    return (
+      <figure className={styles.figure}>
+        <a className={styles.link} href={instagramUrl}>
+          <img src={imageUrl} />
+        </a>
 
-      <figcaption>
-        <a href={`https://instagram.com/${author}`}>{`@${author}`}</a>
-      </figcaption>
-    </figure>
+        <figcaption>
+          <a href={`https://instagram.com/${author}`}>{`@${author}`}</a>
+        </figcaption>
+      </figure>
+    );
+  }
+
+  return (
+    <a
+      href={instagramUrl}
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ display: 'inline-block', marginBottom: 10 }}
+    >
+      Посмотреть пост в Instagram
+    </a>
   );
 };
 
